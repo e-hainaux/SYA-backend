51c9c8ead3f1e970c98aa91033262a5d703374e6
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-03-22 09:47:48 +0100

    back tentative avortée recaptcha

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var express = require("express");
+require("dotenv").config();
+//const expressRecaptcha = require("express-recaptcha"); // Ajout de l'import pour express-recaptcha
+
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-require("dotenv").config();
-
 var indexRouter = require("./routes/index");
-
 var formRouter = require("./routes/form");
 var app = express();
 
 const cors = require("cors");
 app.use(cors());
+
+//const recaptchaSecretKey = process.env.RECAPTCHA_SECRET_KEY; // Récupération de la clé secrète reCAPTCHA
+
+// Configuration de express-recaptcha
+//app.use(expressRecaptcha.middleware.recaptcha({
+//  secretKey: recaptchaSecretKey,
+//}));
 
 app.use(logger("dev"));
 app.use(express.json());
--- routes/form.js
+++ routes/form.js
@@ -1,46 +1,65 @@
 const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
+//const verifyRecaptcha = require("../verifyRecaptcha.mjs"); // Ajout de l'import pour verifyRecaptcha
 require("dotenv").config();
 
-// Endpoint API pour gérer l'envoi d'e-mails
-router.post("/send-email", (req, res) => {
-  const { prenom, nom, societe, email, telephone, commentaire } = req.body;
+// Fonction pour envoyer un email
+const sendEmail = async (options) => {
+  try {
+    // Créer un transporteur SMTP réutilisable
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  // Créer un transporteur SMTP réutilisable à l'aide de transporter
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    port: 587,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+    // Envoyer l'e-mail
+    await transporter.sendMail(options);
+  } catch (error) {
+    console.error("Erreur lors de l'envoi de l'email:", error);
+    throw error;
+  }
+};
+
+// Route pour envoyer un email
+router.post("/send-email", async (req, res) => {
+  const { token, prenom, nom, societe, email, telephone, commentaire } =
+    req.body;
+
+  // **Vérification du token reCAPTCHA**
+  //const isTokenValid = await verifyRecaptcha(token);
+
+  //if (isTokenValid) {
+  // Le token est valide, traiter le formulaire
 
   // Définir les options de l'e-mail
-  let mailOptions = {
+  const mailOptions = {
     from: process.env.EMAIL_USER,
     to: process.env.EMAIL_USER,
     subject: "SYA YOGA - Nouvelle demande de contact",
     html: `<p>Nom: ${prenom} ${nom}</p>
-               <p>Société: ${societe}</p>
-               <p>Email: ${email}</p>
-               <p>Téléphone: ${telephone}</p>
-               <p>Commentaire: ${commentaire}</p>`,
+      <p>Société: ${societe}</p>
+      <p>Email: ${email}</p>
+      <p>Téléphone: ${telephone}</p>
+      <p>Commentaire: ${commentaire}</p>`,
   };
 
   // Envoyer l'e-mail
-  transporter
-    .sendMail(mailOptions)
-    .then(() => {
-      console.log("Email envoyé");
-      res.status(200).json({ message: "Email envoyé avec succès" });
-    })
-    .catch((error) => {
-      console.error("Erreur lors de l'envoi de l'email:", error);
-      res.status(500).json({ error: "Erreur lors de l'envoi de l'email" });
-    });
+  try {
+    await sendEmail(mailOptions);
+    res.status(200).json({ message: "Email envoyé avec succès" });
+  } catch (error) {
+    res.status(500).json({ error: "Erreur lors de l'envoi de l'email" });
+  }
+  // } else {
+  // Le token est invalide, afficher une erreur
+  // res.status(401).json({ error: "Le reCAPTCHA n'est pas valide." });
+  // }
 });
 
 module.exports = router;
